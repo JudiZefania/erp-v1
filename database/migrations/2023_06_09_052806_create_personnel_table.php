056726d47c1f8396538366bced30dab926b99265
Judith Zefania <judizefania@gmail.com>
2023-06-16 12:00:11 +0300

    2023_06_09_052806_create_personnel_table.php

--- database/migrations/2023_06_09_052806_create_personnel_table.php
+++ database/migrations/2023_06_09_052806_create_personnel_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('personnel', function (Blueprint $table) {
             $table->id();
-            $table->stringn('nom');
+            $table->string('nom');
             $table->string('prenom');
             $table->string('poste');
             $table->string('departement');
